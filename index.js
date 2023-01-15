374915efc8b6f7ddd00774089f746c281ddd9f3f
dbedrossian <danny.bedrossian@gmail.com>
2023-01-15 16:19:54 -0500

    worked on inquirer and html

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Employee = require('./lib/Employee');
 
 const employee = new Employee();
 
+const team = [];
+
 const managerQuestions = () => {
     return inquirer.prompt([
         {
@@ -28,7 +30,8 @@ const managerQuestions = () => {
         }
     ])
     .then((answers) => {
-        console.log(answers);
+        team.push(answers);
+        console.log(team);
         buildTeam();
     })
 };
@@ -67,7 +70,9 @@ const buildTeam = () => {
                     }
                 ])
                 .then((answers) => {
-                    console.log(answers);
+                    // console.log(answers);
+                    team.push(answers);
+                    console.log(team);
                     buildTeam();
                 })
             if (answers.Category === 'Add an Intern')
@@ -94,16 +99,71 @@ const buildTeam = () => {
                     }
                 ])
                 .then((answers) => {
-                    console.log(answers);
+                    team.push(answers);
+                    console.log(team);
                     buildTeam();
                 });
             if (answers.Category === 'Finish building your team')
-                generateHTML();
+                finish(team);
         })
 };
 
-const generateHTML = () => {
+const generateHTML = (team) => {
+    console.log(team);
+    const name = team.name;
+    const id = team.id;
+    const role = team.role;
+    const email = team.email;
+    const github = team.github;
     console.log('generate team');
+    console.log(team[0].name);
+ `   <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile Generator</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma-rtl.min.css">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <header class="header123">My Team</header>
+  
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <div
+                    id="cardContainer"
+                        class="card pb-6 shadow-2xl rounded-xl is-cursor-pointer transform is-duration-300 hover-translate-y">
+                        <div class="card-content has-text-left">
+                            <div class="content">
+                                <h3 class="is-size-3 mb-5">${name}</h3>
+                                <h3 class="is-size-4 mb-5">${role}</h3>
+                                <p class="is-size-6 has-text-weight-normal">ID: ${id}</p>
+                                <p class="is-size-6 has-text-weight-normal">Email: ${email}</p>
+                                <p class="is-size-6 has-text-weight-normal">Github ${github}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="/index.js"></script>
+</body>
+
+</html>`
 };
+
+const finish = (team) => {
+    console.log(team);
+    fs.writeFile('newRender/index.html', generateHTML(team));
+    console.log(team);
+    console.log("Success!");
+}
 
 managerQuestions();
