9e38465765fd40bf9e6b33cf290822d5b935998c
dbedrossian <danny.bedrossian@gmail.com>
2023-01-15 14:54:38 -0500

    added inquirer logic

--- index.js
+++ index.js
@@ -1,59 +1,109 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Employee = require('./lib/Employee');
 
-inquirer
-    .prompt([
+const employee = new Employee();
+
+const managerQuestions = () => {
+    return inquirer.prompt([
         {
             type: 'input',
-            message: 'What is the name of the employee?',
-            name: 'Name',
+            name: 'name',
+            message: "What is the team manager's name?",
         },
         {
             type: 'input',
-            message: 'What is the ID?',
-            name: 'ID',
+            name: 'id',
+            message: "What is their employee ID?",
         },
         {
             type: 'input',
-            message: 'What is their email?',
-            name: 'Email',
+            name: 'email',
+            message: "What is their email?",
         },
         {
-            type: 'list',
-            message: 'What type of employee?',
-            name: 'Category',
-            choices: ['Manager', 'Engineer', 'Intern'],
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is their office number?",
         }
     ])
     .then((answers) => {
-        if (answers.Category === 'Manager')
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    message: 'What is their office number?',
-                    name: 'Office',
-                }
-            ]);
-        if (answers.Category === 'Engineer')
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    message: 'What is their Github username?',
-                    name: 'Github',
-                }
-            ]);
-        if (answers.Category === 'Intern')
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    message: 'What is their school?',
-                    name: 'School',
-                }
-            ]);
-            // inquirer.then((answers) => {
-            //     fs.writeFile('index.html', answers.School, (err) =>
-            //         err ? console.error(err) : console.log('Success!'));
-            // })};
-        }
-    )
+        console.log(answers);
+        buildTeam();
+    })
+};
 
+const buildTeam = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Add an employee or Finish building team:',
+            name: 'Category',
+            choices: ['Add an Engineer', 'Add an Intern', 'Finish building your team'],
+        }
+    ])
+        .then((answers) => {
+            if (answers.Category === 'Add an Engineer')
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: "What is their name?",
+                    },
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: "What is their employee ID?",
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: "What is their email?",
+                    },
+                    {
+                        type: 'input',
+                        message: 'What is their Github username?',
+                        name: 'Github',
+                    }
+                ])
+                .then((answers) => {
+                    console.log(answers);
+                    buildTeam();
+                })
+            if (answers.Category === 'Add an Intern')
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: "What is their name?",
+                    },
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: "What is their employee ID?",
+                    },
+                    {
+                        type: 'input',
+                        name: 'email',
+                        message: "What is their email?",
+                    },
+                    {
+                        type: 'input',
+                        message: 'What is their school?',
+                        name: 'school',
+                    }
+                ])
+                .then((answers) => {
+                    console.log(answers);
+                    buildTeam();
+                });
+            if (answers.Category === 'Finish building your team')
+                generateHTML();
+        })
+};
+
+const generateHTML = () => {
+    console.log('generate team');
+};
+
+managerQuestions();
