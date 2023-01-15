11bc63aea3d888f1536d55fc435b9582e1e46414
dbedrossian <danny.bedrossian@gmail.com>
2023-01-15 16:27:04 -0500

    tweaked index.js file

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 const employee = new Employee();
+const manager = new Manager();
+const engineer = new Engineer();
+const intern = new Intern();
 
 const team = [];
 
